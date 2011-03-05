070f9187416ae61916c31f229fd05a5672888842
Eric Kidd <git@randomhacks.net>
2011-03-05 10:42:33 -0500

    tests: Generate a test project with a single, empty test
    We have an annoying lifecycle bug that we'd like to track down, which
    can theoretically be done using tests.  We also want to add tests for
    new features and for any future regressions that we encounter, because
    (a) I like test-driven development, and (b) hand-testing the application
    is getting a bit tedious.

---
+++ tests/src/com/gluegadget/hndroid/hd/test/MainActivityTest.java
@@ -0,0 +1,17 @@
+package com.gluegadget.hndroid.hd.test;
+
+import com.gluegadget.hndroid.hd.MainActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+
+	public MainActivityTest() {
+		super("com.gluegadget.hndroid.hd", MainActivity.class);
+	}
+	
+	public void testPlaceholder() {
+		// TODO: Replace with a real test.
+	}
+}
