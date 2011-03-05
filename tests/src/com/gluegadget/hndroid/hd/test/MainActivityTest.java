6e52434f45cae540985fdb5d8728427edcd8b3f3
Eric Kidd <git@randomhacks.net>
2011-03-05 11:08:18 -0500

    tests: Assert that we we show the correct tab and fragment on launch

--- tests/src/com/gluegadget/hndroid/hd/test/MainActivityTest.java
+++ tests/src/com/gluegadget/hndroid/hd/test/MainActivityTest.java
@@ -1,17 +1,35 @@
 package com.gluegadget.hndroid.hd.test;
 
 import com.gluegadget.hndroid.hd.MainActivity;
+import com.gluegadget.hndroid.hd.WelcomeFragment;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class MainActivityTest extends
 		ActivityInstrumentationTestCase2<MainActivity> {
 
+	MainActivity activity;
+	FragmentManager fragmentManager;
+	
 	public MainActivityTest() {
 		super("com.gluegadget.hndroid.hd", MainActivity.class);
 	}
 	
-	public void testPlaceholder() {
-		// TODO: Replace with a real test.
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		activity = getActivity();
+		fragmentManager = activity.getFragmentManager();
+	}
+
+	private Fragment getDetailsFragment() {
+		return fragmentManager.findFragmentById(com.gluegadget.hndroid.hd.R.id.hnDetailsFrame);
+	}
+
+	public void testShouldSelectFirstTabAndDisplayWelcomeFragmentOnLaunch() {
+		assertEquals(0, activity.getActionBar().getSelectedNavigationIndex());
+		assertTrue(getDetailsFragment() instanceof WelcomeFragment);
 	}
 }
